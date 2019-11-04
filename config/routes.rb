4279352b21be21dd3bc1262edb3e9c75ff09a395
juliana geller <bablouchka@gmail.com>
2019-11-04 12:49:57 +0100

    stupid coaching, no styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
